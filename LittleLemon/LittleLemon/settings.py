4cb319697b2c10d084a8374a0f859de962026591
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-14 22:53:55 +0100

    installing and configuring both of [django debug toolbar,django rest_framework]

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'debug_toolbar',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = "LittleLemon.urls"
@@ -115,3 +118,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
