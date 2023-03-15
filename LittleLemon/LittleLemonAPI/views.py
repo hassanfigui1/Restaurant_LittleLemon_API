3b8afbb44219b615bd9e73d8361009725101d1f0
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-15 13:14:49 +0100

    adding MenuItemView

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -3,3 +3,7 @@ from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 # Create your views here.
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
