819f4bacae2f57f2922fab2b35d74ed06d6f1421
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-15 13:39:37 +0100

    creating SingleMenuItemView class

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -7,3 +7,7 @@ from .serializers import MenuItemSerializer
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
