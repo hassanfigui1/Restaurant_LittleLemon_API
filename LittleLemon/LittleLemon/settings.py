aaee443f72b4cd6876fa80a4c89eb2a2faa4d608
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-22 23:58:08 +0000

    configuring cors

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -51,8 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-        'corsheaders.middleware.CorsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.common.CommonMiddleware",
+
 ]
 
 ROOT_URLCONF = "LittleLemon.urls"
